37507689d7e80b9b887aeb6acb1b29fc9137e657
Sarah E <36022128+sapo83@users.noreply.github.com>
2018-10-23 09:20:23 -0500

    add library function

--- python/random.seq.py
+++ python/random.seq.py
@@ -2,8 +2,17 @@
 
 import random, string
 
+### function with list of letters predefined
 def randomseq(length):
    letters = ['A', 'C', 'G', 'T', '-']
    return ''.join(random.choice(letters) for i in range(length))
 
 print(randomseq(10))
+
+### function where the list of letters can be passed as an argument
+def randomseqlibrary(length, library):
+    letters = library
+    return ''.join(random.choice(letters) for i in range(length))
+
+base = ['A', 'C', 'G', 'T']
+print(randomseqlibrary(10, base))
